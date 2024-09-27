1864520ae470ab6f68a40d072a11b564f4c2ea89
NileshGupta <nilesh6380@gmail.com>
2024-09-26 19:35:56 -0700

    added request mapping

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,8 +1,13 @@
 package src.main.java.com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @GetMapping("/greeting")
+    public String hello() {
+        return "Hello World";
+    }
 }
